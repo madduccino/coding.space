baff75a138a185e1a6bdeaa87a264f6075eb2439
abbreviatedman <colin.jaffe@gmail.com>
2018-05-11 13:22:22 -0400

    finishes full code

--- web/code/quick-clicker/index.js
+++ web/code/quick-clicker/index.js
@@ -9,10 +9,16 @@ document.addEventListener('DOMContentLoaded', function() {
     function countDown() {
         if (active) {
             secondsLeft = secondsLeft - 1;
+
+            displaySeconds(secondsLeft);
+
             if (secondsLeft === 0) {
                 active = false;
                 firstGame = false;
                 clearInterval(updateInterval)
+                setClickButtonText("Time's UP!!")
+                setStartButtonText('Try again?')
+                setPrompt('You can click quicker than that...');
             }
         }
     }
@@ -21,13 +27,21 @@ document.addEventListener('DOMContentLoaded', function() {
         if (active) {
             numClicks = numClicks + 1;
         }
+
+        displayClicks(numClicks);
     }
 
     function startGame() {
         setPrompt('Quick! Click!');
 
+        setClickButtonText('Click Me!');
+
         if (updateInterval) {
             clearInterval(updateInterval);
+        }
+
+        if (firstGame) {
+
         }
         
         firstGame = false;
@@ -35,19 +49,50 @@ document.addEventListener('DOMContentLoaded', function() {
         secondsLeft = 10;
         numClicks = 0;
 
+        displaySeconds(secondsLeft);
+        displayClicks(numClicks);
+
         updateInterval = setInterval(countDown, 1000);
     }
 
+    function displayClicks(clickCount) {
+        var clickCountArea = document.querySelector('.num-clicks');
+
+        clickCountArea.textContent = clickCount;
+    }
+
+    function displaySeconds(secondsCount) {
+        var secondsDisplayArea = document.querySelector('.seconds-left');
+
+        secondsDisplayArea.textContent = secondsCount;
+    }
+
+
     function setPrompt(text) {
         var prompt = document.querySelector('.prompt')
-        var promptText = document.createTextNode(text);
-        prompt.appendChild(promptText) 
+        prompt.textContent = text;
+    }
+
+    function setClickButtonText(text) {
+        var clickButton = document.querySelector('.click-button');
+        clickButton.textContent = text;
+    }
+
+    function setStartButtonText(text) {
+        var startButton = document.querySelector('.start-button');
+        startButton.textContent = text;
     }
     
     function setUpGame() {
         setPrompt('How quick can you click?')
+
         var startButton = document.querySelector('.start-button');
+        var clickButton = document.querySelector('.click-button')
+
         startButton.addEventListener('click', startGame);
+        setStartButtonText('Try it out!');
+
+        clickButton.addEventListener('click', updateCount);
     }
 
     
