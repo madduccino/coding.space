fd068263d76d10733d622d9e0c14ad9320a53670
Maddy Carter <madduccino@gmail.com>
2019-04-22 22:36:27 +0000

    add Scratch cat loader

--- scratch/index.js
+++ scratch/index.js
@@ -1,16 +1,47 @@
-	function loadiframe(step, el) {
-	  var iframe = document.getElementById(step);
-	  iframe.style.display = "block";
-	  el.style.display = "none";
-	  var data = iframe.getAttribute("data-src");
-      iframe.setAttribute("src", data); 
+	function goBack() {
+	 var back = document.createElement("a");
+	  back.href = "./";
+	  back.id = "back";
+	  back.innerHTML = "Back"
+	  var h1 = document.querySelector("h1");
+	  document.querySelector(".container").insertBefore(back, document.querySelector(".container").firstChild)
 	}
 	
 	
-	function loadfirst() {
-	  var iframe = document.getElementById("full");
+	function loadiframe(id, el) {
+	  
+	  var iframe = document.getElementById(id);
 	  var data = iframe.getAttribute("data-src");
-      iframe.setAttribute("src", data); 
+	  iframe.setAttribute("src", data); 
+	  
+	  var block;
+
+	  if (el) {
+	  	el.style.display = "none";
+	  	block = el.parentNode;;
+	  } else {
+	  	block = document.querySelector(".block");
+	  }
+	 
+	  var x = document.createElement("p");
+      x.innerHTML = '<img id="loading" src="./images/costume1.svg"><br>Loading...';
+
+      block.insertBefore(x, block.lastElementChild);
+	  
+       
+      iframe.onload = function() { 
+      	
+      	block.removeChild(x)
+      	iframe.style.display = "block";
+      	iframe.style.width = "100%";
+        iframe.style.height = "402px";
+
+      };
+	
 	}
 	
-	window.onload = loadfirst;
+	
+	window.onload = function() {
+		loadiframe("full");
+		goBack();
+	}
