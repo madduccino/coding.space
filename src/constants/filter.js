100e862b156a71f5e7e12955e3504cbff28333bc
madduccino <madduccino@gmail.com>
2023-12-31 14:38:11 -0500

    add python category

--- src/constants/filter.js
+++ src/constants/filter.js
@@ -3,6 +3,8 @@ export const INTRO_TO_SCRATCH = "Intro to Scratch";
 export const SCRATCH = "Scratch";
 export const WOOF = "Woof";
 export const WEB = "Web";
+export const PYTHON = "Python";
+
 export const STUDENT = "Student";
 export const SCRATCHJR = "Scratch Jr.";
 export const CODE4CHANGE = "Code 4 Change";
