8d57054261c059037943620d54041c480822979a
madduccino <madduccino@gmail.com>
2024-01-28 18:27:01 -0500

    add challenges

--- src/constants/filter.js
+++ src/constants/filter.js
@@ -4,7 +4,7 @@ export const SCRATCH = "Scratch";
 export const WOOF = "Woof";
 export const WEB = "Web";
 export const PYTHON = "Python";
-
+export const CHALLENGES = "Challenges";
 export const STUDENT = "Student";
 export const SCRATCHJR = "Scratch Jr.";
 export const CODE4CHANGE = "Code 4 Change";
